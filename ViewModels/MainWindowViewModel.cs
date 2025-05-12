8cd5dd4aa77d3e1ce4bacbf97399f5b5afbb82b2
GrafMorchan <114511145+GrafMorchan@users.noreply.github.com>
2025-05-12 13:51:56 +0500

    Добавлено

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Avalonia;
 using LibraryCoordinatorApp.Models;
 using LibraryCoordinatorApp.Models.Data;
 using ReactiveUI;
@@ -10,10 +11,36 @@ namespace LibraryCoordinatorApp.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public MainWindowViewModel()
+        private List<Author> _authors = DataWorker.GetAllAuthors();
+        public List<Author> Authors
         {
-
+            get => _authors;
         }
+
+        private List<Book> _books = DataWorker.GetAllBooks();
+        public List<Book> Books
+        {
+            get => _books;
+        }
+
+        private List<BookCopy> _bookCopies = DataWorker.GetAllBookCopies();
+        public List<BookCopy> BookCopies
+        {
+            get => _bookCopies;
+        }
+
+        private List<Genre> _genres = DataWorker.GetAllGenres();
+        public List<Genre> Genres
+        {
+            get => _genres;
+        }
+
+        private List<Publisher> _publishers = DataWorker.GetAllPublishers();
+        public List<Publisher> Publishers
+        {
+            get => _publishers;
+        }
+
 
     }
 }
